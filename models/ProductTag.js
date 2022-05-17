a5ee0dc92a3dc643bf7de063fecf647f86ac080e
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-17 10:35:04 -0600

    created product routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -17,15 +17,13 @@ ProductTag.init(
       type: DataTypes.INTEGER,
       references: {
         model: 'product',
-        key: 'id',
-        unique: false
+        key: 'id'
       },
       tag_id: {
         type: DataTypes.INTEGER,
         references: {
           model: 'tag',
-          key: 'id',
-          unique: false
+          key: 'id'
         },
       }
     }
